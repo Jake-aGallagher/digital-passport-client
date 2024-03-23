7acb4506bb901bff369d8bf09cf3768640e70c28
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-03-23 23:02:19 +0000

    Login, Signup and Logout buttons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,29 @@
+'use client';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [loggedIn, setLoggedIn] = useState(true);
     return (
         <nav className="fixed top-0 left-0 right-0 h-12 md:text-xl text-white flex flex-row justify-start md:justify-center items-center gap-4 md:gap-12">
-            <Link href="/" className='ml-2 md:ml-0'>
+            <Link href="/" className="ml-2 md:ml-0">
                 <span className="hover:text-blue-300 transition-all">Home</span>
             </Link>
             <Link href="/passports">
                 <span className="hover:text-blue-300 transition-all">Passports</span>
             </Link>
-            <button className="absolute right-16 md:right-32 rounded-lg h-10 w-20 flex flex-col justify-center items-center hover:text-blue-300 transition-all">Signup</button>
-            <button className="absolute right-0 md:right-12 rounded-lg h-10 w-20 flex flex-col justify-center items-center hover:text-blue-300 transition-all">Login</button>
+            {loggedIn ? (
+                <button onClick={() => setLoggedIn(false)} className="absolute right-0 md:right-12 rounded-lg h-10 w-20 flex flex-col justify-center items-center hover:text-blue-300 transition-all">Logout</button>
+            ) : (
+                <>
+                    <Link href="/signup" className="absolute right-16 md:right-32 rounded-lg h-10 w-20 flex flex-col justify-center items-center">
+                        <span className="hover:text-blue-300 transition-all">Signup</span>
+                    </Link>
+                    <Link href="/login" className="absolute right-0 md:right-12 rounded-lg h-10 w-20 flex flex-col justify-center items-center">
+                        <span className="hover:text-blue-300 transition-all">Login</span>
+                    </Link>
+                </>
+            )}
         </nav>
     );
 };
