c5508672b0fc7aea48e8b6b21bc8d2556cc44aac
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-03-23 09:17:26 +0000

    Responsive navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,14 +2,15 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav className="fixed top-0 left-0 right-0 h-12 text-xl text-white flex flex-row justify-center items-center gap-12">
-            <Link href="/">
+        <nav className="fixed top-0 left-0 right-0 h-12 md:text-xl text-white flex flex-row justify-start md:justify-center items-center gap-4 md:gap-12">
+            <Link href="/" className='ml-2 md:ml-0'>
                 <span className="hover:text-blue-300 transition-all">Home</span>
             </Link>
             <Link href="/passports">
                 <span className="hover:text-blue-300 transition-all">Passports</span>
             </Link>
-            <button className="absolute right-12 rounded-lg h-10 pb-1 w-20 flex flex-col justify-center items-center hover:text-blue-300 transition-all">Login</button>
+            <button className="absolute right-16 md:right-32 rounded-lg h-10 w-20 flex flex-col justify-center items-center hover:text-blue-300 transition-all">Signup</button>
+            <button className="absolute right-0 md:right-12 rounded-lg h-10 w-20 flex flex-col justify-center items-center hover:text-blue-300 transition-all">Login</button>
         </nav>
     );
 };
