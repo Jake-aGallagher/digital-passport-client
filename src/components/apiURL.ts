fda71dc11b41d6cc94bfe6ce4cbb25098d65840b
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-04-22 21:03:54 +0100

    remove unused env variable and input production routing

--- src/components/apiURL.ts
+++ src/components/apiURL.ts
@@ -1,1 +1,1 @@
-export const SERVER_URL = process.env.NEXT_PUBLIC_DEVELOPMENT === 'TRUE' ? 'http://localhost:7071/api' : 'https://my-live-server-address-here.com';
+export const SERVER_URL = process.env.NEXT_PUBLIC_DEVELOPMENT === 'TRUE' ? 'http://localhost:7071/api' : process.env.NEXT_PUBLIC_SERVER_URL;
